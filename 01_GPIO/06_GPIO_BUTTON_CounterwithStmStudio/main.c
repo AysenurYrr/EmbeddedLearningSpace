6e85cd0a071b7ed26ebd3c14547d4dfbec04f5c3
Ayşenur Yörür <92437276+AysenurYrr@users.noreply.github.com>
2022-05-07 23:14:46 +0300

    Add files via upload

---
+++ 01_GPIO/06_GPIO_BUTTON_CounterwithStmStudio/main.c
@@ -0,0 +1,48 @@
+#include "stm32f10x.h"                  // Device header
+
+uint16_t buttonState=0; 
+uint16_t counter=0;
+void gpioConfig(){
+	
+	GPIO_InitTypeDef GPIOInitStructure;
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE); //for led
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE); //for button
+	
+	//LED
+	GPIOInitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
+	GPIOInitStructure.GPIO_Pin=GPIO_Pin_0;
+	GPIOInitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIOInitStructure);
+	
+	
+	//BUTTON
+	GPIOInitStructure.GPIO_Mode=GPIO_Mode_Out_PP;
+	GPIOInitStructure.GPIO_Pin=GPIO_Pin_6;
+	GPIOInitStructure.GPIO_Speed=GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC, &GPIOInitStructure);
+}
+
+void delay(uint32_t time){
+
+	while(time--);
+	
+
+}
+
+int main() {
+	gpioConfig();
+	while(1){
+		buttonState=GPIO_ReadInputDataBit(GPIOC,GPIO_Pin_6);
+		if (buttonState==1){
+			counter++;
+			delay(3600000);
+			
+			}
+			
+			
+		}
+	
+	
+	}
+
